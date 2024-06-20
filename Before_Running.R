199bc75ec3286c44b36002efeaa7db6645f4745a
Camden-Bergquist <camdentbergquist@gmail.com>
2024-06-20 12:06:12 -0500

    Created a `Before_Running.R` script that will install the app's dependencies for a new user.

---
+++ Before_Running.R
@@ -0,0 +1,10 @@
+# Run this before running the app for the first time.
+## Installs all of the dependencies that the application uses.
+## If prompted to install other dependent packages, accept.
+
+install.packages("Shiny")
+install.packages("tidyverse")
+install.packages("shinydashboard")
+install.packages("DT")
+install.packages("plotly")
+install.packages("shinycssloaders")
